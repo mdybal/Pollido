73e7fe3b38838712850e741041caa85af3ce2ba6
mdybal <mdybal@live.com>
2025-03-03 22:41:03 +0100

    CalendarPoll

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -2,7 +2,12 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export const Button = React.forwardRef<HTMLButtonElement, React.ButtonHTMLAttributes<HTMLButtonElement>>(
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost";
+  size?: "default" | "sm" | "lg";
+}
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, variant = "default", size = "default", ...props }, ref) => {
     return (
       <button
